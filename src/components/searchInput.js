5c6095a8ee3bf673a30a1b0fa55611b4f68e4202
jmiralles <jmirallespaez@gmail.com>
2018-10-16 07:23:49 +0200

    search buttons fixed

--- src/components/searchInput.js
+++ src/components/searchInput.js
@@ -1,7 +1,7 @@
 const searchInput = () => {
-    return `<form class="level">
+  return `<form class="level">
                 <div class="search-control">
-                    <div class="field is-grouped">
+                    <div class="field has-addons">
                         <div class="control">
                             <input class="input" type="text" id="site-search" name="search-input"
                                 placeholder="Search game...">
@@ -10,7 +10,7 @@ const searchInput = () => {
                             <a class="button is-primary" id="searchButton">Search</a>
                         </div>
                         <div class="control">
-                            <a class="button is-light" id="clearSearchButton">Clear Search</a>
+                            <a class="button is-light" id="clearSearchButton">Clear</a>
                         </div>
                     </div>
                 </div>
